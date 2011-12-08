b011571802ca47e0a10decda560e590d8640749e
Jason Hood <jadoxa@yahoo.com.au>
2011-12-08 11:34:12 +1000

    Added v2.00 release binaries.

--- cmdkey.cmd
+++ cmdkey.cmd
@@ -1,7 +1,7 @@
-:: Having CMDkey.exe in AutoRun causes a noticeable slow down in some batch
+:: Having cmdkey.exe in AutoRun causes a noticeable slow down in some batch
 :: files (not due to anything it does, just the very act of loading it).
 :: Running a batch file has no such drawback, so use this to detect if the first
-:: argument is "/C", in which case there's no need to load CMDkey.exe.
+:: argument is "/C", in which case there's no need to load cmdkey.exe.
 @SetLocal
 :: Strip quotes, since FOR really doesn't like it when they're unbalanced.
 @set "cmdline=%CMDCMDLINE:"=%"
